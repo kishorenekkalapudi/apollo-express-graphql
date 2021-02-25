0873c04bdef48f5e4627cfbb4d0bed889c6ed80f
kishor nekkalapudi <knekkalapudi@walmart.com>
2021-02-25 15:03:12 -0800

    add cors and prettier

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+var cors = require("cors");
 
 // Some fake data
-const resetData =[ {dessert: "Oreo",nutritionInfo: {calories: 437,fat: 18, carb: 63,   protein: 4, } },
- {dessert: "Nougat", nutritionInfo: {calories: 360,fat: 19, carb: 50,   protein: 37, } }
+const resetData = [
+  {
+    dessert: "Oreo",
+    nutritionInfo: { calories: 437, fat: 18, carb: 63, protein: 4 },
+  },
+  {
+    dessert: "Nougat",
+    nutritionInfo: { calories: 360, fat: 19, carb: 50, protein: 37 },
+  },
 ];
-let Desserts =[ {dessert: "Oreo",nutritionInfo: {calories: 437,fat: 18, carb: 63,   protein: 4, } },
- {dessert: "Nougat", nutritionInfo: {calories: 360,fat: 19, carb: 50,   protein: 37, } }
+let Desserts = [
+  {
+    dessert: "Oreo",
+    nutritionInfo: { calories: 437, fat: 18, carb: 63, protein: 4 },
+  },
+  {
+    dessert: "Nougat",
+    nutritionInfo: { calories: 360, fat: 19, carb: 50, protein: 37 },
+  },
 ];
 
 // The GraphQL schema in string form
@@ -28,37 +43,36 @@ const typeDefs = `
 
 // The resolvers
 const resolvers = {
-  Query: { Desserts: () => Desserts,
-
-},
-  Mutation:{
-    createDessert:(parent, {dessert,fat,calories,carb,protein})=>{
-        let available=false;
-        Desserts.map((e,i)=>{
-            if(e.dessert===dessert){
-                available=true;
-                e.nutritionInfo={fat,calories,carb,protein}
-            }
-            return e;
-        })
-        const Dessert={ dessert,fat,calories,carb,protein};
-        console.log(available);
-        if(!available)
-        Desserts.push({dessert,nutritionInfo:{fat,calories,carb,protein}})
-        return Dessert;
+  Query: { Desserts: () => Desserts },
+  Mutation: {
+    createDessert: (parent, { dessert, fat, calories, carb, protein }) => {
+      let available = false;
+      Desserts.map((e, i) => {
+        if (e.dessert === dessert) {
+          available = true;
+          e.nutritionInfo = { fat, calories, carb, protein };
+        }
+        return e;
+      });
+      const Dessert = { dessert, fat, calories, carb, protein };
+      console.log(available);
+      if (!available)
+        Desserts.push({
+          dessert,
+          nutritionInfo: { fat, calories, carb, protein },
+        });
+      return Dessert;
     },
-    deleteDessert:(parent, {dessert})=>{
-        data=Desserts.filter((e,i)=>{
-            if(e.dessert!==dessert){
-                return e;
-            }
-            
-        })
-        Desserts=data;
-        return dessert;
-    }
-
-  }
+    deleteDessert: (parent, { dessert }) => {
+      data = Desserts.filter((e, i) => {
+        if (e.dessert !== dessert) {
+          return e;
+        }
+      });
+      Desserts = data;
+      return dessert;
+    },
+  },
 };
 
 // Put together a schema
@@ -69,14 +83,15 @@ const schema = makeExecutableSchema({
 
 // Initialize the app
 const app = express();
+app.use(cors());
 
 // The GraphQL endpoint
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 
 // GraphiQL, a visual editor for queries
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 // Start the server
 app.listen(3000, () => {
-  console.log('Go to http://localhost:3000/graphiql to run queries!');
+  console.log("Go to http://localhost:3000/graphiql to run queries!");
 });
